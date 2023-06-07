8ee3353d5e9bc4fd1fb45cb077a1b6a67dfbbfb8
jonasrochabr <jonasrochabr@gmail.com>
2023-06-07 17:13:07 -0400

    html css complete

---
+++ script.js
@@ -0,0 +1,14 @@
+// capturando elementos HTML e atribuir a variáveis
+
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    console.log(weight, height)
+}
